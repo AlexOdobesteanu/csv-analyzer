1c71df4c4b8150b533c4d95e3e5e0a12e835e673
Alexandru Odobesteanu <odo@Alexandrus-MacBook-Pro.local>
2023-07-02 19:15:48 +0300

    Full project

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { MatchReader } from "./MatchReader";
+import { Summary } from "./Summary";
+
+//Create an instance of MatchReader and pass in something satisfying
+const matchReader = MatchReader.fromCsv("football.csv");
+matchReader.load();
+
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
+
+summary.buildAndPrintReport(matchReader.matches);
